1ec42cae0c220de2020ab5473c480600f5dedcd4
Edwin Josué <54474913+EdwinJosue0124@users.noreply.github.com>
2022-10-23 10:01:16 -0600

    subida archivos guia 10

---
+++ guia10/Market/apiusuario.php
@@ -0,0 +1,28 @@
+<?php
+Header('Access-Control-Allow-Origin:*');
+if($_GET)
+{ $comando=$_GET['comando'];
+	$servername = "localhost";
+	$username = "id19754467_mydb";
+	$password = "anbxzq%y#c6~lZ0!";
+	$dbname = "id19754467_datos";
+	// Crear la conexión
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+    	die("Connection failed: " . $conn->connect_error);
+    	}  
+	if($comando=='autenticar')
+    	{ $usuario=$_GET["usuario"];
+    	$contrasena=$_GET["contrasena"];
+    	$sql = "Select * from usuarios where usuario='$usuario' and contrasena='$contrasena'";
+    	$result=$conn->query($sql);
+    	if($result->num_rows>0){
+        	echo'{"encontrado":"si"}';
+    	}else{
+        	echo'{"encontrado":"no"}';
+    	}
+	}
+    	$conn->close();
+}
+?>
